1fc84d2c8b60a3fda8793209c975e651d143d62a
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2023-05-02 18:39:42 +0200

    computing disparity

--- src/shaders/disparity_cs.hlsl
+++ src/shaders/disparity_cs.hlsl
@@ -5,10 +5,64 @@ RWTexture2D<float> disparityTex : register(u1);
 #define BRIGHTNESS_STANDARD(col) dot(col, float3(0.333333f, 0.333333f, 0.333333f)); // using standard greyscale
 #define BRIGHTNESS_REAL(col) dot(col, float3(0.299f, 0.587f, 0.114f)); // using luminance construction
 
+float4 get_gradients(int3 texPos, int tapSize) {
+    // cam-specific filters
+    float p[] = { 0.229879f, 0.540242f, 0.229879f };
+    float d[] = { -0.425287f, 0.000000f, 0.425287f };
+    
+    // lightfield derivatives
+    float Lx = 0.0f, Ly = 0.0f;
+    float Lu = 0.0f, Lv = 0.0f;
+
+    int nCams = 3; // cams in one dimension
+    int nPixels = tapSize; // pixels in one dimension
+    int pixelOffset = nPixels / 2;
+    
+    // iterate over 2D patch of pixels (3x3)
+    for (int x = 0; x < nPixels; x++)
+    {
+        for (int y = 0; y < nPixels; y++)
+        {
+            for (int u = 0; u < nCams; u++)
+            {
+                for (int v = 0; v < nCams; v++)
+                {
+                    int camIndex = u * 3 + v;
+                    int3 texOffset = int3(x - pixelOffset, y - pixelOffset, camIndex);
+
+                    float3 color = lightField[uint3(texPos + texOffset)].rgb;
+                    float luma = BRIGHTNESS_STANDARD(color);
+                    
+                    // approximate derivatives using 3-tap filter
+                    Lx += d[x] * p[y] * p[u] * p[v] * luma;
+                    Ly += p[x] * d[y] * p[u] * p[v] * luma;
+                    Lu += p[x] * p[y] * d[u] * p[v] * luma;
+                    Lv += p[x] * p[y] * p[u] * d[v] * luma;
+                }
+            }
+        }
+    }
+    
+    return float4(Lx, Ly, Lu, Lv);
+}
+float2 get_disparity(float4 gradients) {
+    // get disparity and confidence
+    float a = gradients.x * gradients.z + gradients.y * gradients.w;
+    float confidence = gradients.x * gradients.x + gradients.y * gradients.y;
+    float disparity = a / confidence;
+    return float2(disparity, confidence);
+}
+
 [numthreads(1, 1, 1)]
 void main(uint3 threadIdx : SV_DispatchThreadID)
-{
-    float4 color = lightField[uint3(threadIdx.x, threadIdx.y, 8)];
-    float greyscale = BRIGHTNESS_STANDARD(color.rgb);
-    disparityTex[threadIdx.xy] = greyscale;
+{  
+    uint3 texPos = uint3(threadIdx.x, threadIdx.y, 8);
+
+    float4 gradients = get_gradients(texPos, 3);
+    // calc disparity and confidence
+    float a = gradients.x * gradients.z + gradients.y * gradients.w;
+    float confidence = gradients.x * gradients.x + gradients.y * gradients.y;
+    float disparity = a / confidence;
+
+    disparityTex[threadIdx.xy] = disparity;
 }
--- src/shaders/swapchain_write_ps.hlsl
+++ src/shaders/swapchain_write_ps.hlsl
@@ -1,7 +1,16 @@
 Texture2D<float> disparityTex : register(t0);
 
+float4 get_heat(float val)
+{
+    // heatmap view (https://www.shadertoy.com/view/WslGRN)
+    float heatLvl = val * 3.14159265 / 2;
+    return float4(sin(heatLvl), sin(heatLvl * 2), cos(heatLvl), 1.0f);
+}
+
 float4 main(float4 inputPos : SV_Position) : SV_Target
 {
-    uint2 pos = uint2(inputPos.x, inputPos.y);
-    return disparityTex[pos].rrrr;
+    uint2 texPos = uint2(inputPos.x, inputPos.y);
+    float disparity = disparityTex[texPos];
+    // return get_heat(disparity);
+    return disparity;
 }
