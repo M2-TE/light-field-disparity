4fcf477f31097e5ebf3e63c3b59224cc5c831950
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2023-05-02 17:35:59 +0200

    Loading lightfield into 3D texture

--- src/renderer/image_wrapper.hpp
+++ src/renderer/image_wrapper.hpp
@@ -5,6 +5,7 @@
 class ImageWrapper 
 {
 public:
+    // ImageWrapper(const char* file_or_foldername) // TODO
     ImageWrapper(vk::Format colorFormat = vk::Format::eR8G8B8A8Srgb) : colorFormat(colorFormat) {}
     ~ImageWrapper() = default;
 
@@ -52,13 +53,134 @@ public:
 				.setBaseMipLevel(0).setLevelCount(1));
 		commandBuffer.pipelineBarrier(vk::PipelineStageFlagBits::eTopOfPipe, vk::PipelineStageFlagBits::eTransfer, {}, {}, {}, barrier);
     }
+   
+   
+    void load_buffer(DeviceWrapper& device, vma::Allocator& allocator, vk::CommandPool& commandPool, vk::Buffer buffer) {
+        vk::CommandBufferAllocateInfo allocInfo = vk::CommandBufferAllocateInfo()
+            .setLevel(vk::CommandBufferLevel::ePrimary)
+            .setCommandPool(commandPool)
+            .setCommandBufferCount(1);
 
-    void load_pfm(const char* filename) {
-		int width, height, channels;
-		stbi_uc* img = stbi_load(filename, &width, &height, &channels, STBI_rgb_alpha);
-		// vk::DeviceSize fileSize = width * height * STBI_rgb_alpha;
-		vk::DeviceSize fileSize = width * height * channels; // same as above?
+        vk::CommandBuffer commandBuffer;
+        vk::Result res = device.logicalDevice.allocateCommandBuffers(&allocInfo, &commandBuffer);
+        if (res != vk::Result::eSuccess) VMI_ERR("Failed to allocate command buffer");
+
+        // begin recording to temporary command buffer
+        vk::CommandBufferBeginInfo beginInfo = vk::CommandBufferBeginInfo()
+            .setFlags(vk::CommandBufferUsageFlagBits::eOneTimeSubmit);
+        commandBuffer.begin(beginInfo);
+
+        // TODO: describe subresource here, need to select each image in lightfield array individually
+        vk::BufferImageCopy region = vk::BufferImageCopy()
+            // buffer
+            .setBufferRowLength(extent.width)
+            .setBufferImageHeight(extent.height)
+            .setBufferOffset(0)
+            // img
+            .setImageExtent(extent)
+            .setImageOffset(0)
+            .setImageSubresource(vk::ImageSubresourceLayers()
+                .setAspectMask(vk::ImageAspectFlagBits::eColor)
+                .setBaseArrayLayer(0).setLayerCount(1)
+                .setMipLevel(0));
+        
+        transition_layout(commandBuffer, vk::ImageLayout::eUndefined, vk::ImageLayout::eTransferDstOptimal);
+        commandBuffer.copyBufferToImage(buffer, image, vk::ImageLayout::eTransferDstOptimal, region);
+        commandBuffer.end();
+
+        vk::SubmitInfo submitInfo = vk::SubmitInfo()
+            .setCommandBufferCount(1)
+            .setPCommandBuffers(&commandBuffer);
+        device.transferQueue.submit(submitInfo);
+        device.transferQueue.waitIdle(); // TODO: change this to wait on a fence instead (upon queue submit) so multiple memory transfers would be possible
+
+        // free command buffer directly after use
+        device.logicalDevice.freeCommandBuffers(commandPool, commandBuffer);
     }
+    void load2D(DeviceWrapper& device, vma::Allocator& allocator, vk::CommandPool& commandPool, const char* filename) {
+		int width, height, srcChannels;
+		stbi_uc* pImg = stbi_load(filename, &width, &height, &srcChannels, STBI_rgb_alpha);
+		vk::DeviceSize fileSize = width * height * STBI_rgb_alpha;
+        
+        // staging buffer
+		vk::BufferCreateInfo bufferInfo = vk::BufferCreateInfo()
+			.setSize(fileSize)
+			.setUsage(vk::BufferUsageFlagBits::eTransferSrc);
+		vma::AllocationCreateInfo allocCreateInfo = vma::AllocationCreateInfo()
+			.setUsage(vma::MemoryUsage::eAuto)
+			.setFlags(vma::AllocationCreateFlagBits::eHostAccessSequentialWrite | vma::AllocationCreateFlagBits::eMapped);
+		vma::AllocationInfo allocInfo;
+		auto stagingBuffer = allocator.createBuffer(bufferInfo, allocCreateInfo, allocInfo);
+
+		// copy into staging buffer
+		memcpy(allocInfo.pMappedData, pImg, fileSize);
+
+        load_buffer(device, allocator, commandPool, stagingBuffer.first);
+
+		// clean up
+		allocator.destroyBuffer(stagingBuffer.first, stagingBuffer.second);
+		stbi_image_free(pImg);
+    }
+    void load3D(DeviceWrapper& device, vma::Allocator& allocator, vk::CommandPool& commandPool, 
+            const char* foldername, const char* commonFilename, std::vector<int> imageIndices) {
+
+        // iterate over all files/folders
+        const std::filesystem::path sandbox{foldername};
+        uint32_t nFiles;
+        // find number of directory entires
+        for (auto const& dirEntry : std::filesystem::directory_iterator{sandbox}) { 
+            nFiles++; 
+        }
+        // reserve space in array for said entries
+        std::vector<std::string> files;
+        files.reserve(nFiles);
+        // iterate again
+        for (auto const& dirEntry : std::filesystem::directory_iterator{sandbox}) {
+            std::string file = dirEntry.path();
+            // only add file to array if it contains given substring
+            if (file.find(commonFilename) != std::string::npos) {
+                files.push_back(file);
+            }
+        }
+        // resize array to proper size and sort entries
+        files.shrink_to_fit();
+        std::sort(files.begin(), files.end());
+        // only load the requested images
+        for (int i = 0; i < imageIndices.size(); i++) {
+            files[i] = files[imageIndices[i]];
+        }
+        files.resize(imageIndices.size());
+
+        // total/slice buffer sizes
+        vk::DeviceSize fileSize = extent.width * extent.height * STBI_rgb_alpha;
+        vk::DeviceSize totalSize = extent.width * extent.height * STBI_rgb_alpha * extent.depth;
+
+        // staging buffer
+		vk::BufferCreateInfo bufferInfo = vk::BufferCreateInfo()
+			.setSize(totalSize)
+			.setUsage(vk::BufferUsageFlagBits::eTransferSrc);
+		vma::AllocationCreateInfo allocCreateInfo = vma::AllocationCreateInfo()
+			.setUsage(vma::MemoryUsage::eAuto)
+			.setFlags(vma::AllocationCreateFlagBits::eHostAccessSequentialWrite | vma::AllocationCreateFlagBits::eMapped);
+		vma::AllocationInfo allocInfo;
+		auto stagingBuffer = allocator.createBuffer(bufferInfo, allocCreateInfo, allocInfo);
+        
+		// copy into staging buffer
+        int srcWidth, srcHeight, srcChannels;
+        char* pBuffer = reinterpret_cast<char*>(allocInfo.pMappedData);
+        for (int i = 0; i < files.size(); i++) {
+            stbi_uc* pImg = stbi_load(files[i].c_str(), &srcWidth, &srcHeight, &srcChannels, STBI_rgb_alpha);
+            // copy into temp buffer with fileSize as stride
+            memcpy(pBuffer + i * fileSize, pImg, fileSize);
+		    stbi_image_free(pImg);
+        }
+
+        load_buffer(device, allocator, commandPool, stagingBuffer.first);
+
+		// clean up
+		allocator.destroyBuffer(stagingBuffer.first, stagingBuffer.second);
+    }
+
 public:
     vk::Image get_image() { return image; }
     vk::ImageView get_image_view() { return imageView; }
@@ -98,7 +220,7 @@ private:
 		// colors view
 		imageView = device.logicalDevice.createImageView(imageViewInfo);
     }
-
+    
 public:
     const vk::Format colorFormat;
 
--- src/shaders/disparity_cs.hlsl
+++ src/shaders/disparity_cs.hlsl
@@ -2,10 +2,13 @@
 Texture3D<float4> lightField : register(t0);
 RWTexture2D<float> disparityTex : register(u1);
 
+#define BRIGHTNESS_STANDARD(col) dot(col, float3(0.333333f, 0.333333f, 0.333333f)); // using standard greyscale
+#define BRIGHTNESS_REAL(col) dot(col, float3(0.299f, 0.587f, 0.114f)); // using luminance construction
+
 [numthreads(1, 1, 1)]
 void main(uint3 threadIdx : SV_DispatchThreadID)
 {
-    float4 color = lightField[uint3(threadIdx.x, threadIdx.y, 0)];
-    float greyscale = color.x / 3.0f + color.y / 3.0f + color.z / 3.0f;
+    float4 color = lightField[uint3(threadIdx.x, threadIdx.y, 8)];
+    float greyscale = BRIGHTNESS_STANDARD(color.rgb);
     disparityTex[threadIdx.xy] = greyscale;
 }
