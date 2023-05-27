e523a23b9ab995c9fe7d3763cc7979c26f7e7a85
Jan <33833587+M2-TE@users.noreply.github.com>
2023-05-27 18:35:32 +0200

    added vma implementation

--- src/pch.cpp
+++ src/pch.cpp
@@ -1,5 +1,8 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 
+#define VMA_IMPLEMENTATION
+#include "vma/include/vk_mem_alloc.hpp"
+
 // storage for dynamic dispatch loader
 VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
